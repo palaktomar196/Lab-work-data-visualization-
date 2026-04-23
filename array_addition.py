f8207ac8c0e7d13733299b2f8750e1225d40e3e8
palaktomar196 <palakthakurs2006@gmail.com>
2026-04-23 10:40:16 +0530

    Create array_addition.py

---
+++ array_addition.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# first array
+array1 = np.array([10,30,40,50])
+
+# second array
+array2 = np.array([90,70,30,20])
+
+#----------------------------
+
+print("First Array : ")
+print(array1)
+print("----------------------------")
+
+print("Second Array : ")
+print(array2)
+print("----------------------------")
+
+#----------------------------
+# addition of array
+# use array3 = np.add(array1,array2)
+#----------------------------
+
+array3 = np.add(array1,array2)
+
+print("Sum of both array : ")
+print(array3)
