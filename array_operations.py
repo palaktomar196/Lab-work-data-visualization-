dcef157013429991f21d04b980fb160738e0d68d
palaktomar196 <palakthakurs2006@gmail.com>
2026-04-23 10:41:17 +0530

    Create array_operations.py

---
+++ array_operations.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# first array
+array1 = np.array([10,30,40,50])
+
+# second array
+array2 = np.array([90,70,30,20])
+#_________________________________
+print("First Array : ")
+print(array1)
+print("----------------------------")
+
+print("Second Array : ")
+print(array2)
+print("----------------------------")
+#___________________________________
+# addition of array
+# use array3 = np.add(array1,array2)
+
+array3 = np.add(array1,array2)
+
+print("Sum of both array : ")
+print(array3)
+print("----------------------------")
+
+# subtraction of array
+# use array4 = np.subtract(array1,array2)
+
+array4 = np.subtract(array1,array2)
+
+print("Difference of both array : ")
+print(array4)
